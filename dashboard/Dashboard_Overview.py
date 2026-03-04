41ff30a38d8cc34a49c44fa34790e8608ff3fd05
Alicja Szulc <a.szulc.903@studms.ug.edu.pl>
2026-03-04 09:20:24 +0100

    add new metrics

--- dashboard/Dashboard_Overview.py
+++ dashboard/Dashboard_Overview.py
@@ -5,6 +5,7 @@ import plotly.express as px
 import datetime as dt
 from utils.filters import apply_filters, init_global_filters, sidebar_filters
 from utils.load import load_glucose
+import altair as alt
 
 st.set_page_config(page_title="CGM Dashboard", layout="wide")
 st.title("CGM Dashboard")
@@ -15,21 +16,6 @@ filters = sidebar_filters(g, key_prefix='main')
 g_f = apply_filters(g, filters)
 
 st.divider()
-
-# TIR Pie
-hypo = (g_f['Conc'] < 70).sum()
-hyper = (g_f['Conc'] > 180).sum()
-normal = len(g_f) - hyper - hypo
-data = pd.DataFrame({
-    'Kategoria': ['hypoglycemia', 'hyperglycemia', 'normoglycemia'],
-    'Wartość': [hypo, hyper, normal]
-})
-fig = px.pie(data, values='Wartość', names='Kategoria', title='Time in Range',
-             color = 'Kategoria',
-             color_discrete_map={'hypoglycemia':'red',
-                                 'hyperglycemia': '#F59127',
-                                 'normoglycemia':'#53C257'},)
-fig.update_layout(width=300, height=300)
 
 # Metric data
 first_month = g['Time'].min().month
@@ -42,16 +28,38 @@ GMI_1st_month = 3.31+0.02392*mean_glycemia_1st_month
 CV = np.std(g_f['Conc']) / mean_glycemia * 100
 CV_1st_month = np.std(g.loc[mask, 'Conc']) / mean_glycemia_1st_month * 100
 
+# TIR Pie
+hypo = (g_f['Conc'] < 70).sum()
+hypo_1st = ((g.loc[mask, 'Conc'] < 70)).sum()
+tbr = (g_f['Conc'] < 70).mean() * 100
+tbr_1st = (g.loc[mask, 'Conc'] < 70).mean() * 100
+hyper = (g_f['Conc'] > 180).sum()
+hyper_1st = (g.loc[mask, 'Conc'] > 180).sum()
+tar = hyper / (g_f['Conc']).count() * 100
+tar_1st = hyper_1st / (g.loc[mask, 'Conc']).count() * 100
+normal = len(g_f) - hyper - hypo
+data = pd.DataFrame({
+    'Kategoria': ['hypoglycemia', 'hyperglycemia', 'normoglycemia'],
+    'Wartość': [hypo, hyper, normal]
+})
+fig = px.pie(data, values='Wartość', names='Kategoria', title='Time in Range',
+             color = 'Kategoria',
+             color_discrete_map={'hypoglycemia':'red',
+                                 'hyperglycemia': '#F59127',
+                                 'normoglycemia':'#53C257'},)
+fig.update_layout(width=300, height=300)
+
+
 st.subheader("Key Glucose Metrics")
-c1, c2, c3, c4 = st.columns(4, gap="medium")
+c1, c2, c3, c4, c5, c6 = st.columns(6, gap="medium")
 with c1:
     st.plotly_chart(fig, use_container_width=False)
 with c2:
     st.markdown("<div style='height:40px'></div>", unsafe_allow_html=True)
     st.metric(
             "Mean glycemia",
-            f"{mean_glycemia:0.1f}mg/L",
-            delta=f"{mean_glycemia - mean_glycemia_1st_month:0.1f}mg/L",
+            f"{mean_glycemia:0.1f}mg/dL",
+            delta=f"{mean_glycemia - mean_glycemia_1st_month:0.1f}mg/dL",
             delta_color="inverse"
         )
 with c3:
@@ -70,7 +78,22 @@ with c4:
             delta=f"{CV - CV_1st_month:0.1f}%",
             delta_color="inverse"
         )
-    
+with c5:
+    st.markdown("<div style='height:40px'></div>", unsafe_allow_html=True)
+    st.metric(
+            "TAR",
+            f"{tar:0.1f}%",
+            delta=f"{tar - tar_1st:0.1f}%",
+            delta_color="inverse"
+        )
+with c6:
+    st.markdown("<div style='height:40px'></div>", unsafe_allow_html=True)
+    st.metric(
+            "TBR",
+            f"{tbr:0.1f}%",
+            delta=f"{tbr - tbr_1st:0.1f}%",
+            delta_color="inverse"
+        )
 st.divider()
 
 # line chart
